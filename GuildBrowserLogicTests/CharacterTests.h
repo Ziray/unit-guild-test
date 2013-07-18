6c581c847f6f0ef4ab20927fcf1cf151ba179f8c
Mike Ziray <mike@zstudiolabs.com>
2013-07-17 23:54:03 -0600

    Added more tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,15 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Michael Ziray on 7/17/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "Character.h"
+
+@interface CharacterTests : SenTestCase
+
+@end
